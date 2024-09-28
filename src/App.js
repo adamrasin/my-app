d07a3926e53e41eb1f77db279e1bf206851d86bf
adamrasin <rasinadam@gmail.com>
2024-09-28 17:24:30 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import placeholderImage from './placeholder.jpg';
 
 function App() {
-  return (
+  const [inputValue, setInputValue] = useState('');
+  const [submittedValue, setSubmittedValue] = useState('');
+
+  const handleSubmit = () => {
+    setSubmittedValue(inputValue);
+  };
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav className="navbar">
+      <ul>
+       <li>Domů</li>
+       <li>O nás</li>
+       <li>Kontakty</li>
+      </ul>
+      </nav>
+      <section className="main-section">
+        <div className="left-section">
+          <h1>Kouzlo Tichého Jezera</h1>
+          <p>Tiché jezero, ukryté v srdci přírody, <br>
+          </br>nabízí klidné útočiště pro každého, <br>
+          </br>kdo touží po odpočinku. <br>
+          </br>Jeho průzračná voda a malebné okolí <br>
+          </br>vytvářejí ideální místo pro relaxaci<br>
+          </br> a objevování krásného kraje. <br>
+          </br>Pro návštěvu se prosím přihlaste.</p>
+          <div className="input-container">
+          <input 
+              type="text" 
+              placeholder="Sem zadejte své udáje"
+              value={inputValue} 
+              onChange={(e) => setInputValue(e.target.value)} 
+            />
+            <button onClick={handleSubmit}>Enter</button>
+          </div>
+          {submittedValue && (
+            <p className="output-text">Byl jsi přihlášen: {submittedValue}</p>
+          )}
+        </div>
+        <div className="right-section">
+        <img src={placeholderImage} alt="d" />
+        </div>
+      </section>
+    </div>  
   );
-}
+
+};
+
 
 export default App;
